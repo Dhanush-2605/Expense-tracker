c344f4487c88b9c860274f67219f0aff65bc646e
dhanush <vellasiridhanush@gmail.com>
2022-03-11 17:58:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+// import ExpenseItem  from './components/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [expense,updateExpense]=useState([]);
+  console.log(NewExpense);
+  // function addItems(){
+         
+  // }
+  //   {
+  //   id:"e1",
+  //   title:"toiler paper",
+  //   amount:32,
+  //   date:new Date()
+  // },
+  // {
+  //   id:"e2",
+  //   title:"paper",
+  //   amount:22,
+  //   date:new Date()
+  // }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <NewExpense />
+
+    <Expenses items={expense} />
+
     </div>
   );
 }
